545520b6145db7c75746032a10c06bd07e362e68
nellizi <masterliz9095@gmail.com>
2022-09-18 14:08:56 +0900

    회원가입 구현

--- src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -1,13 +1,19 @@
 package com.ll.exam.app10.app.member.controller;
 
 
+import com.ll.exam.app10.app.member.Service.MemberService;
+import com.ll.exam.app10.app.member.entity.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
-
+    private final MemberService memberService;
     @GetMapping("/join")
     public String showJoin(){
         return "/member/join";
@@ -15,7 +21,13 @@ public class MemberController {
 
     @PostMapping("/join")
     @ResponseBody
-    public String join(){
+    public String join(String username, String password, String email, MultipartFile profileImg){
+       Member oldmember = memberService.getMemberByUserName(username);
+        if(oldmember != null){
+            return "이미 가입된 회원입니다.";
+        }
+        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+
         return "회원가입 완료";
     }
 }
