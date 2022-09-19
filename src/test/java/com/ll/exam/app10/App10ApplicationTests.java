95e71848f961f589b7c13a937d08a4e537d3c899
nellizi <masterliz9095@gmail.com>
2022-09-19 15:16:58 +0900

    특정 회원으로 로그인 한 경우 테스트 케이스 구현

--- src/test/java/com/ll/exam/app10/App10ApplicationTests.java
+++ src/test/java/com/ll/exam/app10/App10ApplicationTests.java
@@ -2,6 +2,7 @@ package com.ll.exam.app10;
 
 import com.ll.exam.app10.app.home.controller.HomeController;
 import com.ll.exam.app10.app.member.Service.MemberService;
+import com.ll.exam.app10.app.member.controller.MemberController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,5 +54,46 @@ class App10ApplicationTests {
 	void t2() throws Exception {
 		long count = memberService.count();
 		assertThat(count).isGreaterThan(0);
+	}
+	@Test
+	@DisplayName("user1로 로그인 후 프로필페이지에 접속하면 user1의 이메일이 보여야 한다.")
+	@Rollback(false)
+	void t3() throws Exception {
+		// mockMvc로 로그인 처리
+		// WHEN
+		// GET /
+		ResultActions resultActions = mvc
+				.perform(
+						get("/member/profile")
+								.with(user("user1").password("1234").roles("user"))
+				)
+				.andDo(print());
+
+		// THEN
+		// 안녕
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(MemberController.class))
+				.andExpect(handler().methodName("showProfile"))
+				.andExpect(content().string(containsString("user1@test.com")));
+	}
+
+	@Test
+	@DisplayName("user4로 로그인 후 프로필페이지에 접속하면 user4의 이메일이 보여야 한다.")
+	@Rollback(false)
+	void t4() throws Exception {
+
+		ResultActions resultActions = mvc
+				.perform(
+						get("/member/profile")
+								.with(user("user4").password("1234").roles("user"))
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(MemberController.class))
+				.andExpect(handler().methodName("showProfile"))
+				.andExpect(content().string(containsString("user4@test.com")));
 	}
 }
