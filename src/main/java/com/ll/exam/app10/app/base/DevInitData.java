e274d19cd4c1bbe9c55b7828182dcb97122c0601
nellizi <masterliz9095@gmail.com>
2022-09-20 01:43:01 +0900

    개발 테스트용 데이터 생성시 이미지 자동 업로드, 링크로 받은 이미지 확장자 출력하기

--- src/main/java/com/ll/exam/app10/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/app10/app/base/DevInitData.java
@@ -2,6 +2,7 @@ package com.ll.exam.app10.app.base;
 
 
 import com.ll.exam.app10.app.member.Service.MemberService;
+import com.ll.exam.app10.app.member.entity.Member;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +17,12 @@ public class DevInitData {
     CommandLineRunner init(MemberService memberService, PasswordEncoder passwordEncoder) {
         return args -> {
             String password = passwordEncoder.encode("1234");
-            memberService.join("user1", password, "user1@test.com");
-            memberService.join("user2", password, "user2@test.com");
+
+            Member member1 = memberService.join("user1", password, "user1@test.com");
+            memberService.setProfileImgByUrl(member1, "https://picsum.photos/200/300");
+
+            Member member2 = memberService.join("user2", password, "user2@test.com");
+            memberService.setProfileImgByUrl(member2, "https://picsum.photos/200/300");
         };
     }
 }
