ce7c7db4a108d786bbe3d7867aed3ed54edfb529
nellizi <masterliz9095@gmail.com>
2022-09-21 02:48:59 +0900

    이미지 url변경, 수정날짜 세션에 저장하기

--- src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
+++ src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
@@ -2,10 +2,12 @@ package com.ll.exam.app10.app.security.dto;
 
 import com.ll.exam.app10.app.member.entity.Member;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 public class MemberContext extends User implements OAuth2User {
     private final Long id;
     private final String email;
+    @Setter
+    private LocalDateTime modifyDate;
     private final String profileImgUrl;
 
     private Map<String, Object> attributes;
@@ -24,6 +28,7 @@ public class MemberContext extends User implements OAuth2User {
         super(member.getUsername(), member.getPassword(), authorities);
         this.id = member.getId();
         this.email = member.getEmail();
+        this.modifyDate = member.getModifyDate();
         this.profileImgUrl = member.getProfileImgUrl();
     }
 
@@ -46,5 +51,9 @@ public class MemberContext extends User implements OAuth2User {
     @Override
     public String getName() {
         return this.getAttribute(this.userNameAttributeName).toString();
+    }
+
+    public String getProfileImgRedirectUrl() {
+        return "/member/profile/img/" + getId() + "?cacheKey=" + getModifyDate().toString();
     }
 }
