bfcb32238576c1854ffd745ac74e992bd5044f43
nellizi <masterliz9095@gmail.com>
2022-09-21 23:49:34 +0900

    게시글 수정폼에서 이미지 추가

--- src/main/java/com/ll/exam/app10/app/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/app10/app/article/controller/ArticleController.java
@@ -16,7 +16,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartRequest;
@@ -25,6 +24,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.util.Map;
 
+
 @Controller
 @RequestMapping("/article")
 @Slf4j
@@ -32,7 +32,6 @@ import java.util.Map;
 public class ArticleController {
 
     private final ArticleService articleService;
-
     private final GenFileService genFileService;
 
     @PreAuthorize("isAuthenticated()")
@@ -43,20 +42,15 @@ public class ArticleController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/write")
-    public String write(@AuthenticationPrincipal MemberContext memberContext, @Valid ArticleForm articleForm, MultipartRequest multipartRequest, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "article/write";
-        }
+    public String write(@AuthenticationPrincipal MemberContext memberContext, @Valid ArticleForm articleForm, MultipartRequest multipartRequest) {
+
         Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
-
-        log.debug("fileMap : " + fileMap);
-
         Article article = articleService.write(memberContext.getId(), articleForm.getSubject(), articleForm.getContent());
-
 
         RsData<Map<String, GenFile>> saveFilesRsData = genFileService.saveFiles(article, fileMap);
 
         log.debug("saveFilesRsData : " + saveFilesRsData);
+
         String msg = "%d번 게시물이 작성되었습니다.".formatted(article.getId());
         msg = Util.url.encode(msg);
         return "redirect:/article/%d?msg=%s".formatted(article.getId(), msg);
@@ -69,12 +63,6 @@ public class ArticleController {
         model.addAttribute("article", article);
 
         return "article/detail";
-    }
-
-    @GetMapping("/{id}/json/forDebug")
-    @ResponseBody
-    public Article showDetailJson(Model model, @PathVariable Long id) {
-        return articleService.getForPrintArticleById(id);
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -93,12 +81,17 @@ public class ArticleController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/{id}/modify")
-    public String modify(@AuthenticationPrincipal MemberContext memberContext, Model model, @PathVariable Long id, @Valid ArticleForm articleForm) {
+    @ResponseBody
+    public String modify(@AuthenticationPrincipal MemberContext memberContext, Model model, @PathVariable Long id, @Valid ArticleForm articleForm, MultipartRequest multipartRequest) {
         Article article = articleService.getForPrintArticleById(id);
 
         if (memberContext.memberIsNot(article.getAuthor())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
+
+        Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+
+        RsData<Map<String, GenFile>> saveFilesRsData = genFileService.saveFiles(article, fileMap);
 
         articleService.modify(article, articleForm.getSubject(), articleForm.getContent());
 
