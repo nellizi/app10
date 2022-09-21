97b74da2e83ba6b7683abd66d755974deea3ebfa
nellizi <masterliz9095@gmail.com>
2022-09-21 09:51:37 +0900

    게시글 작성구현

--- src/main/java/com/ll/exam/app10/app/article/controller/input/ArticleForm.java
+++ src/main/java/com/ll/exam/app10/app/article/controller/input/ArticleForm.java
@@ -1,7 +1,16 @@
 package com.ll.exam.app10.app.article.controller.input;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
 public class ArticleForm {
+    @NotEmpty
     private String subject;
+    @NotEmpty
     private String content;
 }
