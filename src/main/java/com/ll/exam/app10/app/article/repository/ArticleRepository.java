250d309b88ec15b41a57b12c49190be498a9242b
nellizi <masterliz9095@gmail.com>
2022-09-24 18:31:07 +0900

    ëqueryDsl로 정렬

--- src/main/java/com/ll/exam/app10/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/app10/app/article/repository/ArticleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom   {
 
     List<Article> findAllByOrderByIdDesc();
 }
---
+++ src/main/java/com/ll/exam/app10/app/article/repository/ArticleRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.ll.exam.app10.app.article.repository;
+
+import com.ll.exam.app10.app.article.entity.Article;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+public interface ArticleRepositoryCustom {
+    List<Article> getQslArticlesOrderByIdDesc();
+}
